88ca942bf6384ee70dae4157a11ff535eef73615
Snugglez <32309975+Snugglez@users.noreply.github.com>
2022-02-15 08:26:47 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # frost-flower-death-tear-utils
-Tera Toolbox Module That Uses Minor Hp/Mp Potions to Display Cooldowns of Frost Flower Death/Tear &amp; Optionally Hide Their Effects
+
+Tera Toolbox Module That Uses Minor Hp/Mp Potions to Display Cooldowns of Frost Flower Death/Tear & Optionally Hide Their Effects
+
+![](https://cdn.discordapp.com/attachments/803409013832155177/943134214293979176/unknown.png)
+
+# Commands
+Note, if using the commands in the toolbox channel or `/8`, ignore the `!` prefix
+
+Command | Argument(s) | Example | Description
+---|---|---|---
+**!ffdtu** | NONE | !ffdtu| Toggles the module (on by default)
+**!ffdtu** | hide | !ffdtu hide| Toggles hiding effects (on by default)
---
+++ index.js
@@ -0,0 +1,57 @@
+module.exports = function reee(d) {
+    d.game.initialize('inventory');
+    let tearcd = 4, dreamcd = 8,
+        gloverolls, bootrolls
+
+    d.game.inventory.on('update', () => {
+        try { gloverolls = d.game.inventory.equipment.slots['4'].passivitySets[0].passivities } catch (e) { gloverolls = [] }
+        try { bootrolls = d.game.inventory.equipment.slots['5'].passivitySets[0].passivities } catch (e) { bootrolls = [] }
+        for (let i = 0; i < gloverolls?.length; i++) {
+            if (gloverolls[i] >= 5160270 && gloverolls[i] <= 5160274) tearcd = 8 - gloverolls[i] % 10
+        }
+        for (let i = 0; i < bootrolls?.length; i++) {
+            if ([5160456, 5160457, 5160458].includes(bootrolls[i])) dreamcd = 7
+            if ([5160459, 5160460].includes(bootrolls[i])) dreamcd = 6
+        }
+    })
+
+    function setitemcd(arg, arg2) {
+        d.send('S_START_COOLTIME_ITEM', '*', { item: arg, cooldown: arg2 })
+    }
+
+    d.hook('S_ABNORMALITY_BEGIN', '*', (e) => {
+        if (!d.settings.enabled || !d.settings.hide) return
+        if (d.settings.hide && e.id >= 19171 && e.id <= 19174) return false
+    })
+
+    d.hook('S_EACH_SKILL_RESULT', '*', (e) => {
+        if (!d.settings.enabled) return
+        if ([99930001, 99930002, 99930003].includes(e.skill.id)) setitemcd(6550, dreamcd)
+        if (e.skill.id == 99930000) setitemcd(6560, tearcd)
+        if (!d.settings.hide) return
+        if (e.skill.id >= 99930000 && e.skill.id <= 99930003) return false
+        if (e.skill.id >= 3000 && e.skill.id <= 3005) return false
+    })
+
+    d.hook('S_ACTION_STAGE', '*', (e) => {
+        if (!d.settings.enabled || !d.settings.hide) return
+        if (e.skill.id >= 99930000 && e.skill.id <= 99930003) return false
+        if (e.skill.id >= 3000 && e.skill.id <= 3005) return false
+    })
+
+    d.hook('S_SPAWN_PROJECTILE', '*', (e) => {
+        if (!d.settings.enabled || !d.settings.hide) return
+        if (e.skill.id == 3001 && e.huntingZone == 1023) return false
+    })
+
+    d.command.add('ffdtu', (arg) => {
+        if (!arg) {
+            d.settings.enabled = !d.settings.enabled
+            d.command.message(`${d.settings.enabled ? 'en' : 'dis'}abled`)
+        }
+        if (arg == 'hide') {
+            d.settings.hide = !d.settings.hide
+            d.command.message(`hide mode ${d.settings.hide ? 'en' : 'dis'}abled`)
+        }
+    })
+}
